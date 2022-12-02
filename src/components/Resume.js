35a054cef2771aea65687dce30cde65b9f32d55b
javed97 <javed.shaikh4650@gmail.com>
2022-12-02 12:56:11 +0000

    first commit

---
+++ src/components/Resume.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import LeftContent from './LeftContent';
+import RightContent from './RightContent';
+import '../styles/Resume.scss';
+
+class Resume extends Component {
+  render() {
+    console.log(this.props)
+    return (
+      <div className='resume' id='resume'>
+        <LeftContent data={this.props.data} color={this.props.color} />
+        <RightContent data={this.props.data} color={this.props.color} />
+      </div>
+    );
+  }
+}
+
+export default Resume;
