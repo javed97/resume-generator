9e1fab1eee078017c129a14610bfb54122df9f1d
Javed Shaikh <javed.shaikh4650@gmail.com>
2022-12-13 20:51:21 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,6 +11,4 @@ rm -rf node_modules
 sudo /usr/bin/npm install -y
 sudo /usr/bin/npm run build
 sleep 05
-#sudo /usr/bin/npm start & 
-forever start ./react-scripts 
-npm test
+sudo /usr/bin/npm start &;
