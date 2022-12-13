3726866db1afb7f8183f903fed31c63ffb211843
Javed Shaikh <javed.shaikh4650@gmail.com>
2022-12-13 18:51:46 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,9 +6,9 @@ git clone -b dev https://github.com/javed97/resume-generator.git
 rm -rf node_modules
 sudo apt install npm -y
 cd /home/ubuntu/resume-generator
-sudo npm -g install create-react-app
+sudo /usr/bin/npm -g install create-react-app
 rm -rf node_modules
-sudo npm install -y
-sudo npm run build
+sudo /usr/bin/npm install -y
+sudo /usr/bin/npm run build
 sleep 10
-sudo npm start 
+sudo /usr/bin/npm start 
