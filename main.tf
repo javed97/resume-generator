eb82acde53e4d0f1a3bef2f0fb9212d7301d3576
Javed Shaikh <javed.shaikh4650@gmail.com>
2022-12-15 00:36:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -98,7 +98,7 @@ resource "aws_instance" "app_server" {
     ## scripts = ["./script.sh", "./script.sh"]
     inline = [
        "sudo chmod -R 777 /home/ubuntu/resume-generator/script.sh",
-       "sleep 10", 
+       "sleep 300", 
        "cd /home/ubuntu/resume-generator/",
        "sudo npm start"
    ]
