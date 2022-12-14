a4e4db7e8c25c6cc82705f628e8bf61e6057eb55
Javed Shaikh <javed.shaikh4650@gmail.com>
2022-12-15 00:13:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -88,20 +88,20 @@ resource "aws_instance" "app_server" {
     private_key = file("devops18.pem")
   }
 
- ## provisioner "file" {
-   # source      = "exec_scripts"
-   # destination = "/home/ubuntu"
-  #}
+  provisioner "file" {
+    source      = "exec_scripts"
+    destination = "/home/ubuntu"
+  }
 
 
-  #provisioner "remote-exec" {
-    # scripts = ["./exec_scripts/envsetup.sh", "./exec_scripts/service.sh"]
-    #inline = [
-     #  "sudo chmod -R 777 /home/ubuntu/exec_scripts",
-      # "./exec_scripts/envsetup.sh",
-       #"./exec_scripts/service.sh"
-    #]
-  #}
+  provisioner "remote-exec" {
+     scripts = ["./script.sh", "./script.sh"]
+    inline = [
+       "sudo chmod -R 777 /home/ubuntu/resume-generator/script.sh",
+       "./script.sh",
+      "sudo npm start"
+   ]
+  }
 
   tags = {
     Name = "group18-terraform"
