cdf910c3d917647ed746069537261101de5ca1d5
Javed Shaikh <javed.shaikh4650@gmail.com>
2022-12-14 12:10:55 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,4 +11,7 @@ rm -rf node_modules
 sudo /usr/bin/npm install -y
 sudo /usr/bin/npm run build
 sleep 05
-sudo /usr/bin/npm start &;
+#sudo /usr/bin/npm start &;
+sudo npm install pm2 -g
+cd /build
+sudo pm2 start react-scripts
