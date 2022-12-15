13a1689aa97d75324aab380c73f3bf04f7681e89
Javed Shaikh <javed.shaikh4650@gmail.com>
2022-12-15 14:17:06 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,6 +6,8 @@ git init
 git clone -b dev https://github.com/javed97/resume-generator.git
 cd /home/ubuntu/resume-generator
 sudo /usr/bin/npm -g install create-react-app
+sudo npm install -y
+sudo npm run build
 sudo apt install nginx -y
 cp -R /home/ubuntu/resume-generator/  /var/www/html/
 mv /home/ubuntu/resume-generator/default  /etc/nginx/sites-available/
