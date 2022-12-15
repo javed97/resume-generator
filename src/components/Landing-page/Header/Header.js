63bfbafd40f884cfbdf1be15288c1f12ecf8cb24
Javed Shaikh <javed.shaikh4650@gmail.com>
2022-12-15 16:17:06 +0000

    Update Header.js

--- src/components/Landing-page/Header/Header.js
+++ src/components/Landing-page/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
   const auth = useAuth();
 
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light py-2 sticky-top">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary py-2 sticky-top">
       <Link class="navbar-brand text-info" to="/">Resume Builder</Link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
