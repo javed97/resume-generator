6895404755818118840bf35c91fa269aa57a8b03
Javed Shaikh <javed.shaikh4650@gmail.com>
2022-12-15 13:23:43 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -98,7 +98,7 @@ resource "aws_instance" "app_server" {
     ## scripts = ["./script.sh", "./script.sh"]
     inline = [
        "sudo chmod -R 777 /home/ubuntu/resume-generator/",
-      "sudo . /home/ubuntu/resume-generator/script.sh"
+      ". /home/ubuntu/resume-generator/script.sh"
      ]
   }
 
